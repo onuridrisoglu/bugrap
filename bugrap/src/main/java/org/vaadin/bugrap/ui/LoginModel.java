2cb074b06bfb19d530184109df8d60b0aa93cebb
Onur Idrisoglu <onur@vaadin.com>
2017-11-28 13:58:16 +0200

    Reports View

--- bugrap/src/main/java/org/vaadin/bugrap/ui/LoginModel.java
+++ bugrap/src/main/java/org/vaadin/bugrap/ui/LoginModel.java
@@ -4,11 +4,16 @@ import org.vaadin.bugrap.BaseModel;
 import org.vaadin.bugrap.DatabaseHelper;
 import org.vaadin.bugrap.domain.entities.Reporter;
 
+import com.vaadin.navigator.Navigator;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
 
 public class LoginModel extends BaseModel{
 
+	public LoginModel(Navigator navigator) {
+		super(navigator);
+	}
+	
 	public void login(String un, String pw) {
 		Reporter reporter = getRepository().authenticate(un, pw);
 		if (reporter == null && DatabaseHelper.initializeIfEmpty(getRepository())) {
@@ -17,7 +22,8 @@ public class LoginModel extends BaseModel{
 		}
 		
 		if (reporter != null) {
-			Notification.show("Welcome "+reporter.getName(), Type.TRAY_NOTIFICATION);
+			loginUser = reporter;
+			getNavigator().navigateTo(NAV_REPORT);
 		}else {
 			Notification.show("Please check your credentials", Type.WARNING_MESSAGE);
 		}
