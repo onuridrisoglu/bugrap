a10675760ffe55c983abf8e6af6a38df6f149c3c
Onur Idrisoglu <onur@vaadin.com>
2017-12-05 14:47:31 +0200

    Pull Request[Report Details] Comments

--- bugrap/src/main/java/org/vaadin/bugrap/ui/ReportsModel.java
+++ bugrap/src/main/java/org/vaadin/bugrap/ui/ReportsModel.java
@@ -1,11 +1,10 @@
 package org.vaadin.bugrap.ui;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 import org.vaadin.bugrap.BaseModel;
 import org.vaadin.bugrap.domain.BugrapRepository.ReportsQuery;
@@ -16,100 +15,57 @@ import org.vaadin.bugrap.domain.entities.Report.Priority;
 import org.vaadin.bugrap.domain.entities.Report.Status;
 import org.vaadin.bugrap.domain.entities.Report.Type;
 import org.vaadin.bugrap.domain.entities.Reporter;
-import org.vaadin.bugrap.ui.columns.CamelcaseTextRenderer;
-import org.vaadin.bugrap.util.StringUtil;
 
-import com.vaadin.data.Binder;
 import com.vaadin.data.ValidationException;
 import com.vaadin.navigator.Navigator;
-import com.vaadin.ui.ComboBox;
-import com.vaadin.ui.Grid;
 
 public class ReportsModel extends BaseModel{
 
-	private Project selectedProject;
-	private ProjectVersion selectedVersion;
-	private Binder<Report> reportBinder = new Binder<Report>();
-	
 	public ReportsModel(Navigator navigator) {
 		super(navigator);
 	}
 	
-	public Binder<Report> getBinder(){
-		return reportBinder;
-	}
-	
-	
-	public void fillProjects(ComboBox<Project> combo) {
+	public List<Project> findProjects() {
 		List<Project> projects = new ArrayList<Project>();
 		projects.addAll(getRepository().findProjects());
 		Collections.sort(projects);
-		combo.setItems(projects);
-		combo.setSelectedItem(projects.get(0));
+		return projects;
 	}
 	
-	public void fillPriorties(ComboBox<Priority> combo) {
-		combo.setItems(Priority.values());
+	public Collection<Priority> getPriorties() {
+		return Arrays.asList(Priority.values());
+	}
+	public Collection<Type> getTypes() {
+		return Arrays.asList(Type.values());
+	}
+	public Collection<Status> getStatuses() {
+		return Arrays.asList(Status.values());
 	}
 	
-	public void fillTypes(ComboBox<Type> combo) {
-		combo.setItems(Type.values());
-		combo.setItemCaptionGenerator(type -> StringUtil.converToCamelCaseString(type.toString()));
+	public Collection<Reporter> findReporters() {
+		return getRepository().findReporters();
 	}
 	
-	public void fillStatuses(ComboBox<Status> combo) {
-		combo.setItems(Status.values());
-	}
-
-	public void fillAssigned(ComboBox<Reporter> combo) {
-//		List<Reporter> reporters = new ArrayList<Reporter>();
-//		reporters.addAll();
-//		Collections.sort(reporters);
-		combo.setItems(getRepository().findReporters());
-	}
-	
-	public void processProjectChange(Optional<Project> selectedItem, ComboBox<ProjectVersion> cmbVersion, ComboBox<ProjectVersion> cmbVersionEdit) {
-		selectedProject = selectedItem.get();
+	public List<ProjectVersion> findProjectVersions(Project selectedProject) {
 		List<ProjectVersion> projectVersions = new ArrayList<ProjectVersion>();
 		projectVersions.addAll(getRepository().findProjectVersions(selectedProject));
 		Collections.sort(projectVersions);
-		
-		cmbVersion.setItems(projectVersions);
-		cmbVersionEdit.setItems(projectVersions);
-		cmbVersion.setSelectedItem(projectVersions.get(0));
+		return projectVersions;
 	}
-
-	public void processVersionChange(Optional<ProjectVersion> selectedItem, Grid<Report> gridReports) {
-		boolean isVersionChanged = selectedVersion != selectedItem.get();
-		Set<Report> selectedReportsToRemember = null;
-		if (!isVersionChanged)
-			selectedReportsToRemember = gridReports.getSelectedItems();
-		
-		selectedVersion = selectedItem.get();
-		
+	
+	public Collection<Report> findReports(Project project, ProjectVersion version){
 		ReportsQuery query = new ReportsQuery();
-		query.project = selectedProject;
-		query.projectVersion = selectedVersion;
-		
-		List<Report> reports = new ArrayList<Report>();
-		reports.addAll(getRepository().findReports(query));
-		gridReports.setItems(reports);
-		
-		if (selectedReportsToRemember != null)
-			for (Report seletedReport : selectedReportsToRemember) {
-				gridReports.select(seletedReport);
-			}
+		query.project = project;
+		query.projectVersion = version;
+		return getRepository().findReports(query);
 	}
 
-	public Report updateReport() throws ValidationException {
-		Report report =	 getRepository().save(reportBinder.getBean());
-		return report;
+	public Report saveReport(Report report) throws ValidationException {
+		return getRepository().save(report);
 	}
 
-	public void revertChanges() {
-		Report report = reportBinder.getBean();
-		report = getRepository().getReportById(report.getId());
-		reportBinder.setBean(report);
+	public Report getReportById(long reportId) {
+		return getRepository().getReportById(reportId);
 	}
 
 
--- bugrap/src/main/java/org/vaadin/bugrap/ui/ReportsView.java
+++ bugrap/src/main/java/org/vaadin/bugrap/ui/ReportsView.java
@@ -1,11 +1,17 @@
 package org.vaadin.bugrap.ui;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.vaadin.bugrap.BaseModel;
+import org.vaadin.bugrap.domain.entities.Project;
+import org.vaadin.bugrap.domain.entities.ProjectVersion;
 import org.vaadin.bugrap.domain.entities.Report;
 import org.vaadin.bugrap.ui.columns.FineDateRenderer;
 import org.vaadin.bugrap.ui.generated.ReportsViewBase;
 import org.vaadin.bugrap.util.StringUtil;
 
+import com.vaadin.data.Binder;
 import com.vaadin.data.ValidationException;
 import com.vaadin.event.selection.SelectionEvent;
 import com.vaadin.navigator.View;
@@ -16,10 +22,12 @@ import com.vaadin.ui.Notification.Type;
 public class ReportsView extends ReportsViewBase implements View{
 
 	private ReportsModel model;
+	private Binder<Report> binder = new Binder<Report>();
 	
 	public ReportsView(ReportsModel m) {
 		super();
 		model = m;
+		initializeGrid();
 		initializeUIComponents();
 		initializeBinder();
 	}
@@ -35,14 +43,21 @@ public class ReportsView extends ReportsViewBase implements View{
 	}
 
 	private void refreshComboboxContent() {
-		model.fillProjects(cmbProjectFilter);
-		model.fillPriorties(cmbEditPriority);
-		model.fillTypes(cmbEditType);
-		model.fillStatuses(cmbEditStatus);
-		model.fillAssigned(cmbEditAssignedTo);
+		List<Project> projects = model.findProjects();
+		cmbProjectFilter.setItems(projects);
+		cmbProjectFilter.setSelectedItem(projects.get(0));
+		
+		cmbEditPriority.setItems(model.getPriorties());
+		
+		cmbEditType.setItems(model.getTypes());
+		cmbEditType.setItemCaptionGenerator(type -> StringUtil.converToCamelCaseString(type.name()));
+		
+		cmbEditStatus.setItems(model.getStatuses());
+		
+		cmbEditAssignedTo.setItems(model.findReporters());
 	}
 	
-	private void initializeUIComponents() {
+	private void initializeGrid() {
 		gridReports.removeAllColumns();
 		gridReports.addColumn("priority").setCaption("PRIORITY").setExpandRatio(1);
 		gridReports.addColumn(report -> StringUtil.converToCamelCaseString(report.getType().toString())).setCaption("TYPE").setExpandRatio(1);
@@ -50,32 +65,49 @@ public class ReportsView extends ReportsViewBase implements View{
 		gridReports.addColumn("assigned").setCaption("ASSIGNED TO").setExpandRatio(2);
 		gridReports.addColumn("timestamp", new FineDateRenderer()).setCaption("LAST MODIFIED").setExpandRatio(2);
 		gridReports.addColumn("reportedTimestamp", new FineDateRenderer()).setCaption("REPORTED").setExpandRatio(2);
-		
 //		gridReports.setColumns("priority", "type", "summary", "assigned", "timestamp", "reportedTimestamp");
-		
-		cmbProjectFilter.addSelectionListener(evt -> model.processProjectChange(cmbProjectFilter.getSelectedItem(), cmbVersion, cmbEditVersion));
-		cmbVersion.addSelectionListener(evt -> model.processVersionChange(cmbVersion.getSelectedItem(), gridReports));
+	}
+	
+	private void initializeUIComponents() {
+		cmbProjectFilter.addSelectionListener(evt -> processProjectChange());
+		cmbVersion.addSelectionListener(evt -> processVersionChange());
 		btnUpdateReport.addClickListener(evt -> saveReport());
-		btnRevertReport.addClickListener(evt -> model.revertChanges());
+		btnRevertReport.addClickListener(evt -> revertChanges());
 		btnLogout.addClickListener(evt -> model.logout());
 		gridReports.addSelectionListener(evt -> displaySelectedReport(evt));
 	}
+
+	private void processProjectChange() {
+		List<ProjectVersion> versions = model.findProjectVersions(cmbProjectFilter.getValue());
+		cmbVersion.setItems(versions);
+		cmbEditVersion.setItems(versions);
+	}
 	
+	private void processVersionChange() {
+		refreshGridContent();
+	}
+
+	private void refreshGridContent() {
+		Collection<Report> selectedReports = gridReports.getSelectedItems();
+		gridReports.setItems(model.findReports(cmbProjectFilter.getValue(), cmbVersion.getValue()));
+		if (selectedReports.size() > 0)
+			gridReports.getSelectedItems().addAll(selectedReports);
+	}
 
 	private void initializeBinder() {
-		model.getBinder().bind(cmbEditPriority, Report::getPriority, Report::setPriority);
-		model.getBinder().bind(cmbEditType, Report::getType, Report::setType);
-		model.getBinder().bind(cmbEditStatus, Report::getStatus, Report::setStatus);
-		model.getBinder().bind(cmbEditAssignedTo, Report::getAssigned, Report::setAssigned);
-		model.getBinder().bind(cmbEditVersion, Report::getVersion, Report::setVersion);
-		model.getBinder().bind(txtReportDescription, Report::getDescription, Report::setDescription);
+		binder.bind(cmbEditPriority, Report::getPriority, Report::setPriority);
+		binder.bind(cmbEditType, Report::getType, Report::setType);
+		binder.bind(cmbEditStatus, Report::getStatus, Report::setStatus);
+		binder.bind(cmbEditAssignedTo, Report::getAssigned, Report::setAssigned);
+		binder.bind(cmbEditVersion, Report::getVersion, Report::setVersion);
+		binder.bind(txtReportDescription, Report::getDescription, Report::setDescription);
 	}
 
 	private void displaySelectedReport(SelectionEvent<Report> evt) {
 		float position = 100;
 		if (evt.getFirstSelectedItem().isPresent()) {
 			Report selectedReport = evt.getFirstSelectedItem().get();
-			model.getBinder().setBean(selectedReport);
+			binder.setBean(selectedReport);
 			btnReportSummary.setCaption(selectedReport.getSummary());
 			position = 55;
 		}
@@ -85,12 +117,16 @@ public class ReportsView extends ReportsViewBase implements View{
 	private void saveReport() {
 		Report report;
 		try {
-			report = model.updateReport();
+			report = model.saveReport(binder.getBean());
 			Notification.show("Report["+report.getSummary()+"] is saved", Type.TRAY_NOTIFICATION);
-			model.processVersionChange(cmbVersion.getSelectedItem(), gridReports);
+			refreshGridContent();
 		} catch (ValidationException e) {
 			Notification.show("Missing information, "+e.getMessage(), Type.ERROR_MESSAGE);
-			e.printStackTrace();
 		}
+	}
+	
+	private void revertChanges() {
+		Report report = model.getReportById(binder.getBean().getId());
+		binder.setBean(report);
 	}
 }
